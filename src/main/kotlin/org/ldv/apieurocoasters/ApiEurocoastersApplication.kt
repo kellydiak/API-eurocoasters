6e402c36513017749fda53cbec07b491987d2684
kellydiak <yauth11@gmail.com>
2026-05-05 11:32:47 +0200

    1er commit de la cration d une api

---
+++ src/main/kotlin/org/ldv/apieurocoasters/ApiEurocoastersApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.apieurocoasters
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiEurocoastersApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiEurocoastersApplication>(*args)
+}
---
+++ src/test/kotlin/org/ldv/apieurocoasters/ApiEurocoastersApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.apieurocoasters
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiEurocoastersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
